560df5dcfe67113a76ed722720562eddb0eed136
MounirMessadi <mounirmessadi2004@gmail.com>
2025-12-16 21:27:52 +0100

    creating calc app

--- calc.py
+++ calc.py
@@ -1,1 +1,16 @@
+# app/calculator.py
+
+def calculate(operator: str, a: float, b: float) -> float:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        if b == 0:
+            raise ZeroDivisionError("division par zéro interdite.")
+        return a / b
+    else:
+        raise ValueError(f"{operator} n'est pas un opérateur valide.")
 
